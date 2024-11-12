dac959344087d4fc97176703d9b812c50df8cb64
sophiakobzar <smirthiguna18@gmail.com>
2024-11-12 09:21:50 -0600

    Adding Multifactor Authentication via OTP

--- server.js
+++ server.js
@@ -2,8 +2,12 @@ require('dotenv').config();
 const express = require('express');
 const authRoutes = require('./routes/authRoutes');
 const cors = require('cors');
+const bodyParser = require('body-parser');
 
 const app = express();
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 app.use(express.json());
 app.use(cors());
@@ -12,5 +16,5 @@ app.use('/auth', authRoutes);
 
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
+   console.log(`Server running on http://localhost:${PORT}`);
 }); 
